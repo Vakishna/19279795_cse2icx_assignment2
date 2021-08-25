a82419ca284e363489feb7c2640f64eee24674ce
Vakishna Thayalan <vakisha1@gmail.com>
2021-08-25 21:44:27 +1000

    about toggle

--- js/about.js
+++ js/about.js
@@ -1,12 +1,72 @@
-﻿$("#jarrod").click(function () {
+﻿var jarrod = false;
+var daisy = false;
+var ivy = false;
+var ren = false;
+
+$("#jarrod").click(function () {
     $("#daisyDesc").hide();
-    $("#jarrodDesc").show();
+    $("#ivyDesc").hide();
+    $("#renDesc").hide();
+    daisy = false;
+    ivy = false;
+    ren = false;
+
+    if (jarrod == false) {
+        $("#jarrodDesc").show();        
+        jarrod = true;
+    } else {
+        $("#jarrodDesc").hide();
+        jarrod = false;
+    }   
 });
-
-
-
 
 $('#daisy').click(function () {
     $("#jarrodDesc").hide();
-    $("#daisyDesc").show();
+    $("#ivyDesc").hide();
+    $("#renDesc").hide();
+    jarrod = false;
+    ivy = false;
+    ren = false;
+
+    if (daisy == false) {
+        $("#daisyDesc").show();
+        daisy = true;
+    } else {
+        $("#daisyDesc").hide();
+        daisy = false;
+    }   
+});
+
+$("#ivy").click(function () {
+    $("#jarrodDesc").hide();
+    $("#daisyDesc").hide();
+    $("#renDesc").hide();
+    jarrod = false;
+    daisy = false;
+    ren = false;
+    
+    if (ivy == false) {
+        $("#ivyDesc").show();
+        ivy = true;
+    } else {
+        $("#ivyDesc").hide();
+        ivy = false;
+    }
+});
+
+$("#ren").click(function () {
+    $("#jarrodDesc").hide();
+    $("#daisyDesc").hide();    
+    $("#ivyDesc").hide(); 
+    jarrod = false;
+    daisy = false;
+    ivy = false;
+    
+    if (ren == false) {
+        $("#renDesc").show();
+        ren = true;
+    } else {
+        $("#renDesc").hide();
+        ren = false;
+    }
 });
