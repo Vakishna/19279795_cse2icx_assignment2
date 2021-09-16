97c8f6a8d79d997d2b365fef909bf839d6023f1e
Vakishna Thayalan <vakisha1@gmail.com>
2021-09-16 22:37:51 +1000

    added response + sitemap boilerplate + order items

--- js/order.js
+++ js/order.js
@@ -1,15 +1,12 @@
 $("#itemAdder").click(function() {
+
     var n = $(".item").length;
-    var newItem = n+1;
+    var newItem = n + 1;
+
     $('<div></div>').attr('id', 'item'+newItem).addClass('item').appendTo("#items");
-
     var itemLabel = "<label for=\"item" + newItem + "\">Item " + newItem + ": </label>";
-
     $("#item" + newItem).append(itemLabel);
-
     var itemList  = "<select id=\"item" + newItem + "\" name=\"item" + newItem + "\"><option id=\"eucalyptusCaesia\" value=\"eucalyptusCaesia\">Eucalyptus Caesia</option><option id=\"eucalyptusPauciflora\" value=\"eucalyptusPauciflora\">Eucalyptus Pauciflora</option><option id=\"corymbiaFicifolia\" value=\"corymbiaFicifolia\">Corymbia Ficifolia</option><option id=\"callistemonCitrinus\" value=\"callistemonCitrinus\">Callistemon Citrinus</option></select>";
-
     $("#item" + newItem).append(itemList);
-
 
 });
